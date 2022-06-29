e5ec1a2b21f478c0e49c652d930c82de1f8f4271
lamduy1807 <lamduy0718@gmail.com>
2022-06-29 11:39:32 +0700

    1st upload Maven project

---
+++ src/test/java/com/duylvl/mathutil/core/MathUtilTest.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.duylvl.mathutil.core;
+
+import static com.duylvl.mathutil.core.MathUtil.getFactorial;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ *
+ * @author DuyLVL
+ */
+public class MathUtilTest {
+
+    public static Object[][] initData() {
+        return new Integer[][]{
+            //{input, expected}
+            {1, 1}, {2, 2}, {5, 120}, {6, 120}
+        };
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "initData") //tên hàm cung câp data, ngâm dinh thú tu
+    public void testGetFactorialGivenRightArgumentReturnsWell(int input, long expected) {
+        assertEquals(expected, getFactorial(input));
+    }
+
+//    @Test
+//    public void testGetFactorialGivenRightArgumentReturnsWell() {
+//        assertEquals(120, getFactorial(5));
+//    }
+    //@Test(expected = ExceptionName.class)
+    @Test
+    public void testGetFactorialGivenWrongArgumentThrowsException() {
+//        Executable exceptionObject = () -> {
+//            getFactorial(-5);
+//        };
+
+//        assertThrows(IllegalArgumentException.class, exceptionObject);
+        assertThrows(IllegalArgumentException.class, () -> getFactorial(-5));
+    }
+}
